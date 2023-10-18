094bd946ad0106046610c5da8c07adbba69e7d27
K.Sielski <kris@sielski.com>
2023-10-18 16:43:03 -0400

    Added HW4

---
+++ Class5-MoreFunctions/Assignment4.cpp
@@ -0,0 +1,64 @@
+//Assignment 4:  Functions
+// Area done as an example
+#include <iostream>
+
+using namespace std;
+
+
+//Function FindArea
+int FindArea(int x, int y)
+{
+    return x*y;
+}
+
+//Function FindVolume
+
+//Function FindMaxInt
+
+//Function FindMinInt
+
+//Function FindPower (use for loop)
+//Reminder:  x^0 = 1
+
+
+// -- MAIN --
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    //Find area and output
+    int area = FindArea(3,4);
+
+    //Find volume and output
+    //FindVolume(3,4,5);
+
+
+    //Find Max numbers
+    //FindMaxInt(2,3,4);
+    //FindMaxInt(10,6,9);
+    //FindMaxInt(-2,-1,-4);
+
+
+    //Find Min Numbers
+    //FindMinInt(7,2,4);
+    //FindMinInt(-1,0,1);
+    //FindMinInt(7,6,5);
+
+    //Find Powers
+    //FindPower(2,3);
+    //FindPower(3,2);
+    //FindPower(4,0);
+
+    //Output Area
+     cout << "Area = " << area << endl;
+
+    //Output Volume
+
+    //Output sum of Max numbers
+
+    //Output sum of Min numbers
+
+    //Output sum of powers
+
+    return 0;
+}
